6c170136a5153c9114003249c79c8dfff39c5a90
Suh Donghwi <hwidongsuh@gmail.com>
2021-03-14 21:53:03 +0900

    feat: add population buttons

--- src/components/ConfigModal.tsx
+++ src/components/ConfigModal.tsx
@@ -11,7 +11,6 @@ const ModalContainer = styled.div`
 
   background: rgba(52, 58, 64, 0.4);
   backdrop-filter: blur(20px);
-  -webkit-backdrop-filter: blur(20px);
   border-radius: 10px;
   border: 1px solid rgba(255, 255, 255, 0.18);
 
@@ -21,24 +20,76 @@ const ModalContainer = styled.div`
   width: 28rem;
 `;
 
-const ModalTitle = styled.h1`
-  margin: 0;
+const Title = styled.h1`
+  margin: 0 0 1.5rem 0;
 
   display: flex;
   align-items: center;
 `;
 
+const SettingsGrid = styled.div`
+  display: grid;
+
+  grid-template-columns: 7rem 1fr;
+  column-gap: 1rem;
+  align-items: center;
+`;
+
+const SettingsName = styled.h2`
+  font-weight: normal;
+  font-size: 1rem;
+
+  text-align: center;
+`;
+
+const SettingsProperty = styled.div``;
+
+const Button = styled.button`
+  cursor: pointer;
+
+  background-color: #343a40;
+  backdrop-filter: blur(20px);
+  appearance: none;
+  border: none;
+
+  font-size: 1rem;
+  color: white;
+  padding: 0.3rem 0.7rem;
+  border-radius: 3px;
+  border: 1px solid #495057;
+
+  & + & {
+    margin-left: 0.5rem;
+  }
+`;
+
 export default function ConfigModal() {
   return (
     <ModalContainer>
-      <ModalTitle>
+      <Title>
         <FaUsers
           className={css`
-            margin-right: 0.75rem;
+            margin-right: 0.8rem;
           `}
         />
         Environment settings
-      </ModalTitle>
+      </Title>
+
+      <SettingsGrid>
+        <SettingsName>Number of communities</SettingsName>
+        <SettingsProperty>
+          <p>hello</p>
+        </SettingsProperty>
+
+        <SettingsName>Add population</SettingsName>
+        <SettingsProperty>
+          <Button>1</Button>
+          <Button>5</Button>
+          <Button>10</Button>
+          <Button>50</Button>
+          <Button>100</Button>
+        </SettingsProperty>
+      </SettingsGrid>
     </ModalContainer>
   );
 }
