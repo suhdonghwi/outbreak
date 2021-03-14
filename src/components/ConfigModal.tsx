625702e483c36965340c09d6cc8f7c551ca92a35
Suh Donghwi <hwidongsuh@gmail.com>
2021-03-14 20:36:08 +0900

    feat: add icons in config modal

--- src/components/ConfigModal.tsx
+++ src/components/ConfigModal.tsx
@@ -1,25 +1,44 @@
 import styled from "@emotion/styled";
+import { css } from "@emotion/css";
 
-const Modal = styled.div`
+import { FaUsers } from "react-icons/fa";
+
+const ModalContainer = styled.div`
   position: absolute;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
 
-  background: rgba(73, 80, 87, 0.25);
-  box-shadow: 0 8px 32px 0 #212529;
-  backdrop-filter: blur(17px);
-  -webkit-backdrop-filter: blur(17px);
+  background: rgba(52, 58, 64, 0.4);
+  backdrop-filter: blur(20px);
+  -webkit-backdrop-filter: blur(20px);
   border-radius: 10px;
   border: 1px solid rgba(255, 255, 255, 0.18);
 
-  width: 500px;
-  height: 300px;
-
-  padding: 1rem;
+  padding: 2rem;
   color: white;
+
+  width: 28rem;
+`;
+
+const ModalTitle = styled.h1`
+  margin: 0;
+
+  display: flex;
+  align-items: center;
 `;
 
 export default function ConfigModal() {
-  return <Modal>Hello, world!</Modal>;
+  return (
+    <ModalContainer>
+      <ModalTitle>
+        <FaUsers
+          className={css`
+            margin-right: 0.75rem;
+          `}
+        />
+        Environment settings
+      </ModalTitle>
+    </ModalContainer>
+  );
 }
