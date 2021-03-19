1b91a92dc58059460263a13411f6d64e7b380c89
Suh Donghwi <hwidongsuh@gmail.com>
2021-03-19 10:45:00 +0900

    feat: add text to timeline

--- src/objects/Timeline.ts
+++ src/objects/Timeline.ts
@@ -1,7 +1,10 @@
 import gsap from "gsap/all";
 import * as PIXI from "pixi.js";
+import { TextStyle } from "pixi.js";
 
-export default class Timeline extends PIXI.Graphics {
+export default class Timeline extends PIXI.Container {
+  private _timeline: PIXI.Graphics;
+
   private _drawWidth: number;
   private _divider: number;
 
@@ -14,32 +17,46 @@ export default class Timeline extends PIXI.Graphics {
     this._drawWidth = drawWidth;
     this._divider = divider;
 
-    this.draw();
+    this._timeline = new PIXI.Graphics();
+    this.drawTimeline(1);
+    this.addChild(this._timeline);
   }
 
-  draw() {
-    const tickRadius = 10;
+  drawTimeline(from: number) {
+    const tickRadius = 14;
 
     const drawBigTick = (x: number) => {
-      this.drawCircle(x, 2.5, tickRadius);
+      this._timeline.drawCircle(x, 3.5, tickRadius);
     };
 
-    this.beginFill(0xced4da, 0.2);
-    this.drawRect(0, 0, this._drawWidth, 5);
-    this.endFill();
+    this._timeline.clear();
+    this._timeline.removeChildren();
 
-    this.beginFill(0x868e96);
+    this._timeline.beginFill(0x868e96, 0.5);
+    this._timeline.drawRect(0, 0, this._drawWidth, 7);
+    this._timeline.endFill();
+
+    this._timeline.beginFill(0x495057);
+
+    const textStyle = new PIXI.TextStyle({
+      fill: "white",
+      fontSize: 14,
+    });
+
     let x = 0;
     const delta = this._drawWidth / this._divider;
-    for (let i = 0; i < this._divider - 1; i++) {
-      x += delta;
+
+    for (let i = 0; i < this._divider + 1; i++) {
+      const number = new PIXI.Text((from + i).toString(), textStyle);
+      number.resolution = 2;
+      number.x = x - number.width / 2;
+      number.y -= 5;
+      this._timeline.addChild(number);
 
       drawBigTick(x);
+      x += delta;
     }
-
-    drawBigTick(0);
-    drawBigTick(this._drawWidth - tickRadius);
-    this.endFill();
+    this._timeline.endFill();
   }
 
   show() {
